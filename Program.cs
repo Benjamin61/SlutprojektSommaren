57a0898b0eb1fd1c40ed4198a7764cb7fd5d0cdc
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2020-03-23 10:25:13 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace SlutprojektSommaren
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!!");
 			Console.ReadLine();
 
 		}
