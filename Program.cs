288301152f73ad609a7b752ca2b94bc63dc29fdb
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2020-03-23 10:29:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace SlutprojektSommaren
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!!");
+			Console.WriteLine("Welcome player!");
+			Console.WriteLine("Your goal for the game is to roam in this house and find a key in order to escape!");
+			Console.WriteLine("Where do you want to go?");
 			Console.ReadLine();
 
 		}
